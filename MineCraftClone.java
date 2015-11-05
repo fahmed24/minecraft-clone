5885002349a6640b5e102bcb03b98aaedc1405a1
Sharat Virunchipuram <thesharatv@gmail.com>
2015-11-04 16:38:48 -0800

    First commit

---
+++ MineCraftClone.java
@@ -0,0 +1,56 @@
+package minecraftclone;
+import org.lwjgl.opengl.DisplayMode;
+import static org.lwjgl.opengl.GL11.*;
+import java.util.Scanner;
+import java.io.*;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.glu.GLU;
+
+public class MineCraftClone {
+
+    private FPCameraController fp = new FPCameraController(0f, 0f, 0f);
+    private DisplayMode displayMode;
+
+    public void start() {
+        try {
+            createWindow();
+            initGL();
+            fp.gameLoop();//render();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createWindow() throws Exception {
+        Display.setFullscreen(false);
+        DisplayMode d[] = Display.getAvailableDisplayModes();
+        for (int i = 0; i < d.length; i++) {
+            if (d[i].getWidth() == 640 && d[i].getHeight() == 480 && d[i].getBitsPerPixel() == 32) {
+                displayMode = d[i];
+                break;
+            }
+        }
+        Display.setDisplayMode(displayMode);
+        Display.setTitle("MineCraftClone");
+        Display.create();    }
+
+
+    private void initGL() {
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        GLU.gluPerspective(500.0f, (float) displayMode.getWidth() / (float) displayMode.getHeight(), 0.1f, 500.0f);
+
+        glMatrixMode(GL_MODELVIEW);
+        glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+        
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MineCraftClone mcc = new MineCraftClone();
+        mcc.start();
+    }
+
+}
---
+++ Vector3Float.java
@@ -0,0 +1,10 @@
+package minecraftclone;
+
+public class Vector3Float {
+	private float x, y, z;
+	public Vector3Float(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = y;
+	}
+}
